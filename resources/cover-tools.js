94911850ec2d6fbccf81b296c54c330899ee9e08
Seth Clydesdale <sethc1995@gmail.com>
2018-04-19 15:45:23 -0400

    fix cursor display on PS4

--- resources/cover-tools.js
+++ resources/cover-tools.js
@@ -1557,7 +1557,7 @@
 
       window.open().document.write(
         '<style>'+
-          'body{padding:0;margin:0;background:#000;display:flex;min-height:100vh;flex-direction:column;' + ( PS_Cover.isPS4 ? 'cursor:none' : '' ) + '}'+
+          'body{padding:0;margin:0;background:#000;display:flex;min-height:100vh;flex-direction:column;' + ( PS_Cover.isPS4 ? 'cursor:none!important' : '' ) + '}'+
           '#creation-info{color:#CCC;font-size:16px;font-family:Arial;padding:6px;}'+
           '#cover-result{flex:1 0 auto;text-align:center;}'+
         '</style>'+
